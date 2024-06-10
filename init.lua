919945ffaea8a210a8207a9cc86969ad40ecdc1e
Nick Wolf <WhiteWolf1776@pm.me>
2024-06-10 11:38:27 -0500

    stop wrapping long lines (kills on csv's)

--- init.lua
+++ init.lua
@@ -103,6 +103,8 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 --vim.opt.relativenumber = true
+-- Do not wrap long lines
+vim.opt.wrap = false
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
